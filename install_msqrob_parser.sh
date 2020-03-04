a9e05894e5226f37a266cb6d24df1f8d91cf80fa
Sander Willems <swillems@users.noreply.github.com>
2020-03-04 16:55:18 +0100

    Update install_msqrob_parser.sh

--- install_msqrob_parser.sh
+++ install_msqrob_parser.sh
@@ -2,7 +2,7 @@
 
 if [[ $(conda env list | awk '{print $1}' ) != *"msqrob_parser"* ]]; then  
   git clone https://github.com/swillems/progenesis_histones_to_msqrob.git
-  conda create -n msqrob_parser python=3.8 -c bioconda pyteomics ipykernel -y
+  conda create -n msqrob_parser -c bioconda python=3.8 pyteomics ipykernel -y
 else 
  echo "MsqRob already installed"
 fi;
