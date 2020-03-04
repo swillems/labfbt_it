e56099b72eee28e8112156e7916c0de755f7443b
Sander Willems <swillems@users.noreply.github.com>
2020-03-04 14:28:45 +0100

    Update install_conda.sh

--- install_conda.sh
+++ install_conda.sh
@@ -13,9 +13,6 @@ if ! hash conda 2>/dev/null; then
   conda init
   conda config --set auto_activate_base false
   echo "Installing conda notebook kernels."
-  conda create -n conda_jupyter_notebook_kernels jupyter nb_conda_kernels -y
-  echo "Adding conda_notebooks alias to ~/.bashrc."
-  echo "alias conda_notebooks='conda activate conda_jupyter_notebook_kernels && jupyter notebook && conda deactivate'" >> ~/.bashrc
   echo "alias cmd_get='echo Get a coffee for Maarten'" >> ~/.bashrc
   echo "Cleaning up conda installation."
   rm Miniconda3-latest-Linux-x86_64.sh
