73e947e839d72a0e3470691151b9b68cb078d2bc
Sander Willems <swillems@users.noreply.github.com>
2020-03-04 13:49:57 +0100

    Update install_conda.sh

--- install_conda.sh
+++ install_conda.sh
@@ -18,7 +18,6 @@ if ! hash conda 2>/dev/null; then
   echo "alias conda_notebooks='conda activate conda_jupyter_notebook_kernels && jupyter notebook && conda deactivate'" >> ~/.bashrc
   echo "alias cmd_get='Get a coffee for Maarten'" >> ~/.bashrc
   echo "Cleaning up conda installation."
-  source ~/.bashrc
   rm Miniconda3-latest-Linux-x86_64.sh
   rm install_conda.sh
 else
