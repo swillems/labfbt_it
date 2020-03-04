2fab9e74d2f64f5f231fdbd388548755a917121d
Sander Willems <swillems@users.noreply.github.com>
2020-03-04 15:45:07 +0100

    Update install_ms2pip.sh

--- install_ms2pip.sh
+++ install_ms2pip.sh
@@ -1,6 +1,10 @@
-#!bash
+ #!bash
 
-conda create -n ms2pip python=3.6.4 ipykernel -y
-conda activate ms2pip
-pip install ms2pip
-conda deactivate
+if [[ $(conda env list | awk '{print $1}' ) = *"ms2pip"* ]]; then  
+ conda create -n ms2pip python=3.6.4 ipykernel -y
+ conda activate ms2pip
+ pip install ms2pip
+ conda deactivate
+else 
+ echo "MS2PIP already installed"
+fi;
