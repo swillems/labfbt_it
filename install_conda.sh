f4c4e1448e40e45fa1d0e35d9d45ae683c0366a6
Sander Willems <swillems@users.noreply.github.com>
2020-03-04 11:29:30 +0100

    Update install_conda.sh

--- install_conda.sh
+++ install_conda.sh
@@ -8,12 +8,6 @@ if ! hash conda 2>/dev/null; then
   bash Miniconda3-latest-Linux-x86_64.sh -b -p ~/miniconda3
   echo "Updating conda."
   eval "$(~/miniconda3/bin/conda shell.bash hook)"
-  # echo "Downloading conda."
-  # wget https://repo.anaconda.com/archive/Anaconda3-2019.10-Linux-x86_64.sh
-  # echo "Installing conda."
-  # bash Anaconda3-2019.10-Linux-x86_64.sh -b -p ~/anaconda3
-  # echo "Updating conda."
-  # eval "$(~/anaconda3/bin/conda bash hook)"
   conda update -n root conda -y
   echo "Initializing conda."
   conda init
@@ -26,6 +20,7 @@ if ! hash conda 2>/dev/null; then
   echo "Cleaning up conda installation."
   source ~/.bashrc
   rm Miniconda3-latest-Linux-x86_64.sh
+  rm install_conda.sh
 else
   echo "Conda is already installed."
 fi
