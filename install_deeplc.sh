51f640715792caf2717c23d62b7ece277c22ba8e
Sander Willems <swillems@users.noreply.github.com>
2020-03-04 15:42:06 +0100

    Update install_deeplc.sh

--- install_deeplc.sh
+++ install_deeplc.sh
@@ -1,6 +1,10 @@
  #!bash
 
-conda create -n ms2pip python=3.6.4 -y ipykernel
-conda activate ms2pip
-pip install ms2pip
-conda deactivate
+if [[ $(conda env list | awk '{print $1}' ) = *"ms2pip"* ]]; then  
+ conda create -n ms2pip python=3.6.4 -y ipykernel
+ conda activate ms2pip
+ pip install ms2pip
+ conda deactivate
+else 
+ echo "MS2PIP already installed"
+fi;
