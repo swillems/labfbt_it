76b60860f2e131d4c512df4f79f795ee21ae7607
Sander Willems <swillems@users.noreply.github.com>
2020-03-04 14:39:11 +0100

    Create install_all.sh

---
+++ install_all.sh
@@ -0,0 +1,12 @@
+wget https://raw.githubusercontent.com/swillems/labfbt_it/master/install_conda.sh
+bash install_conda.sh
+source ~/.bashrc
+
+wget https://raw.githubusercontent.com/swillems/labfbt_it/master/install_ms2pip.sh
+bash install_ms2pip.sh
+source ~/.bashrc
+
+wget https://raw.githubusercontent.com/swillems/labfbt_it/master/install_ion_networks.sh
+bash install_ion_networks.sh
+source ~/.bashrc
+
