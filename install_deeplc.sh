5c0cdd35b332d76ff0ed037c5661fb2f55d07586
Sander Willems <swillems@users.noreply.github.com>
2020-03-04 15:45:26 +0100

    Update install_deeplc.sh

--- install_deeplc.sh
+++ install_deeplc.sh
@@ -1,7 +1,7 @@
  #!bash
 
 if [[ $(conda env list | awk '{print $1}' ) = *"deeplc"* ]]; then  
- conda create -n deeplc python=3.6.4 -y ipykernel
+ conda create -n deeplc python=3.6.4 ipykernel -y
  conda activate deeplc
  pip install deeplc
  conda deactivate
