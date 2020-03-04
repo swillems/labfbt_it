098313897a27c49ec023543f0f3deb457c09e9f8
Sander Willems <swillems@users.noreply.github.com>
2020-03-04 13:44:32 +0100

    Update install_conda.sh

--- install_conda.sh
+++ install_conda.sh
@@ -13,7 +13,7 @@ if ! hash conda 2>/dev/null; then
   conda init
   conda config --set auto_activate_base false
   echo "Installing conda notebook kernels."
-  conda create -n conda_jupyter_notebook_kernels jupyter nb_conda_kernels ipykernel -y
+  conda create -n conda_jupyter_notebook_kernels jupyter nb_conda_kernels -y
   echo "Adding conda_notebooks alias to ~/.bashrc."
   echo "alias conda_notebooks='conda activate conda_jupyter_notebook_kernels && jupyter notebook && conda deactivate'" >> ~/.bashrc
   echo "alias cmd_get='Get a coffee for Maarten'" >> ~/.bashrc
