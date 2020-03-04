b298fcb6031435414e44ef8854c10c5b57dfe2fc
Sander Willems <swillems@users.noreply.github.com>
2020-03-04 16:36:24 +0100

    Update install_notebooks.sh

--- install_notebooks.sh
+++ install_notebooks.sh
@@ -1,6 +1,6 @@
 #!bash
 
-if [[ $(conda env list | awk '{print $1}' ) = *"conda_jupyter_notebook_kernels"* ]]; then  
+if [[ $(conda env list | awk '{print $1}' ) != *"conda_jupyter_notebook_kernels"* ]]; then  
   echo "Installing notebooks"
   conda create -n conda_jupyter_notebook_kernels jupyter nb_conda_kernels -y
   echo "alias conda_notebooks='conda activate conda_jupyter_notebook_kernels && jupyter notebook && conda deactivate'" >> ~/.bashrc
@@ -8,7 +8,7 @@ else
  echo "Notebooks already installed"
 fi;
 
-if [[ $(conda env list | awk '{print $1}' ) = *"sandbox"* ]]; then  
+if [[ $(conda env list | awk '{print $1}' ) != *"sandbox"* ]]; then  
   echo "Installing sandbox"
   conda create -n sandbox ipykernel -y 
 else 
