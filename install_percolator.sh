8f3bbd201403a64b25508cbc6cd437946a59d5d6
Sander Willems <swillems@users.noreply.github.com>
2020-03-04 15:51:56 +0100

    Update install_percolator.sh

--- install_percolator.sh
+++ install_percolator.sh
@@ -1,12 +1,21 @@
 #!bash
 
+mkdir percolator
+wget --output-document percolator/percolator_3_02_01_ubuntu.tar.gz https://github.com/percolator/percolator/releases/download/rel-3-02-01/ubuntu64.tar.gz
+tar xzvf percolator/percolator_3_02_01_ubuntu.tar.gz -C percolator
+
 if ! hash percolator 2>/dev/null; then
-  mkdir percolator
-  wget --output-document percolator/percolator_3_02_01_ubuntu.tar.gz https://github.com/percolator/percolator/releases/download/rel-3-02-01/ubuntu64.tar.gz
-  tar xzvf percolator/percolator_3_02_01_ubuntu.tar.gz -C percolator
-  sudo dpkg -i percolator/elude-v3-02-linux-amd64.deb
+  echo "Installing percolator"
   sudo dpkg -i percolator/percolator-v3-02-linux-amd64.deb
-  rm -r percolator
 else
   echo "Percolator is already installed."
 fi
+
+if ! hash elude 2>/dev/null; then
+  echo "Installing elude"
+  sudo dpkg -i percolator/elude-v3-02-linux-amd64.deb
+else
+  echo "Elude is already installed."
+fi
+
+rm -r percolator
