78098eaa142803acf8dca9e2b61af372432645e2
Sander Willems <swillems@users.noreply.github.com>
2020-03-04 17:39:09 +0100

    Update install_all.sh

--- install_all.sh
+++ install_all.sh
@@ -11,34 +11,34 @@ fi
 
 wget https://raw.githubusercontent.com/swillems/labfbt_it/master/install_conda.sh
 bash install_conda.sh
-source ~/.bashrc
+exec bash
 rm install_conda.sh
 
 wget https://raw.githubusercontent.com/swillems/labfbt_it/master/install_notebooks.sh
 bash install_notebooks.sh
-source ~/.bashrc
+exec bash
 rm install_notebooks.sh
 
 wget https://raw.githubusercontent.com/swillems/labfbt_it/master/install_ms2pip.sh
 bash install_ms2pip.sh
-source ~/.bashrc
+exec bash
 rm install_ms2pip.sh
 
 wget https://raw.githubusercontent.com/swillems/labfbt_it/master/install_percolator.sh
 bash install_percolator.sh
-source ~/.bashrc
+exec bash
 rm install_percolator.sh
 
 cd ../software
 
 wget https://raw.githubusercontent.com/swillems/labfbt_it/master/install_ion_networks.sh
 bash install_ion_networks.sh
-source ~/.bashrc
+exec bash
 rm install_ion_networks.sh
 
 wget https://raw.githubusercontent.com/swillems/labfbt_it/master/install_msqrob_parser.sh
 bash install_msqrob_parser.sh
-source ~/.bashrc
+exec bash
 rm install_msqrob_parser.sh
 
 cd ..
