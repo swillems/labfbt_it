71d47176789b378de10d1de0c33bd73c1da0e780
Sander Willems <swillems@users.noreply.github.com>
2020-03-04 14:22:39 +0100

    Update install_conda.sh

--- install_conda.sh
+++ install_conda.sh
@@ -5,7 +5,7 @@ if ! hash conda 2>/dev/null; then
   echo "Downloading conda."
   wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
   echo "Installing conda."
-  bash Miniconda3-latest-Linux-x86_64.sh -b -p ~/miniconda3
+  bash Miniconda3-latest-Linux-x86_64.sh -b -p ~/.miniconda3
   echo "Updating conda."
   eval "$(~/miniconda3/bin/conda shell.bash hook)"
   conda update -n root conda -y
